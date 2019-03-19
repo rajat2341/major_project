82692e229f382151707f214469f9c50770b2106a
rajat2341 <rajat24goel@gmail.com>
2019-03-19 12:38:35 +0530

    compiler done.

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # major_project
 This project is made for college purposes. it is a competitve website which has sections which has coding questions respectively. you can practice check your score and compete with others. compiler support languages like C, C++, Java, Python.
+
+
+Installation steps:
+1. Setting Up Compilers -
+    Inorder to compile any programming language , you need to first have the compiler for that programming language in the server machine.
+
+    1.a. C and C++
+        Installation :You need GCC compiler that can compile programs from your cmd/terminal
+        Windows - You can get MinGw (http://www.mingw.org/).
+        Linux - Most of the linux versions are installed with gcc by default. If you haven't got , Take a look at Installing GCC .
+
+    1.b. Java
+        Installion : You need JDK ( Java Development Kit ) to compile Java programs.Click here to download JDK for various platforms.
+        (https://www.oracle.com/technetwork/java/javase/downloads/jdk8-downloads-2133151.html)
+
+    1.c. Python
+        Installation : You can get and install Python from here(https://www.python.org/downloads/)
--- routes/index.js
+++ routes/index.js
@@ -119,4 +119,86 @@ router.get('/index/logout', function(req, res){
     res.redirect('/');
 });
 
+router.post('/compilecode' , function (req , res ) {
+	var code = req.body.code;	
+	var input = req.body.input;
+    var inputRadio = req.body.inputRadio;
+    var lang = req.body.lang;
+    if((lang === "C") || (lang === "C++"))
+    {        
+        if(inputRadio === "true")
+        {    
+        	var envData = { OS : "windows" , cmd : "g++", options: {timeout:1000 }};	   	
+        	compiler.compileCPPWithInput(envData , code ,input , function (data) {
+        		if(data.error)
+        		{
+        			res.send(data.error);    		
+        		}
+        		else
+        		{
+        			res.send(data.output);
+        		}
+        	});
+	   }
+	   else
+	   {
+	   	var envData = { OS : "windows" , cmd : "g++", options: {timeout:1000 }};	   
+        	compiler.compileCPP(envData , code , function (data) {
+                if(data.error)
+                {
+                    res.send(data.error);
+                }    	
+                else
+                {
+                    res.send(data.output);
+                }
+            });
+	   }
+    }
+    if(lang === "Java")
+    {
+        if(inputRadio === "true")
+        {
+            var envData = { OS : "windows" };     
+            console.log(code);
+            compiler.compileJavaWithInput( envData , code , function(data){
+                res.send(data);
+            });
+        }
+        else
+        {
+            var envData = { OS : "windows" };     
+            console.log(code);
+            compiler.compileJavaWithInput( envData , code , input ,  function(data){
+                res.send(data);
+            });
+
+        }
+
+    }
+    if( lang === "Python")
+    {
+        if(inputRadio === "true")
+        {
+            var envData = { OS : "windows"};
+            compiler.compilePythonWithInput(envData , code , input , function(data){
+                res.send(data);
+            });            
+        }
+        else
+        {
+            var envData = { OS : "windows"};
+            compiler.compilePython(envData , code , function(data){
+                res.send(data);
+            });
+        }
+    }  
+});
+
+router.get('/fullStat' , function(req , res ){
+    compiler.fullStat(function(data){
+        res.send(data);
+    });
+});
+
 module.exports = router;
